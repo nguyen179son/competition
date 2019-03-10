2de13bc74c4b0112b37d17e5f7f576e8a5ccd341
nguyen <sonnc@deha-soft.com>
2019-03-10 21:01:49 +0700

    add currencies api

--- competitionService/routes/api.php
+++ competitionService/routes/api.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/currencies','CurrencyController@index');
 Route::get('/competitions', 'CompetitionController@listCompetition');
 Route::prefix('competition')->group(function () {
     Route::post('/', 'CompetitionController@store');
